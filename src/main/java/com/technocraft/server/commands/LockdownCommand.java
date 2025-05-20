20979c4b0d24061cc12e415ffc85ee0ba3a08194
BuildTools <unconfigured@null.spigotmc.org>
2025-05-20 17:05:27 -0400

    Add maintenance mode

---
+++ src/main/java/com/technocraft/server/commands/LockdownCommand.java
@@ -0,0 +1,79 @@
+package com.technocraft.server.commands;
+
+import com.technocraft.server.Main;
+import com.technocraft.server.commands.announce.AnnounceCommand;
+import com.technocraft.server.commands.global.GlobalCommand;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.time.Duration;
+
+public class LockdownCommand implements CommandExecutor {
+    private Main main;
+
+    public LockdownCommand(Main main)
+    {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args)
+    {
+        if (Bukkit.hasWhitelist())
+        {
+            disableLockdownMode();
+        } else { enableLockdownMode(); }
+        return false;
+
+    }
+
+    private void enableLockdownMode()
+    {
+        new AnnounceCommand().announce("Maintenance Mode", "The server is entering maintenance mode");
+
+        Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+            @Override
+            public void run()
+            {
+                new GlobalCommand().global("The whitelist will be enabled and all server members will be kicked.", true);
+            }
+        }, toTicks(5));
+
+        Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+            @Override
+            public void run()
+            {
+                new GlobalCommand().global("Please check Discord for updates.", true);
+            }
+        }, toTicks(7));
+
+        Bukkit.getScheduler().runTaskLater(main, new Runnable() {
+            @Override
+            public void run()
+            {
+                //whitelist enforced should already be set to true in server.properties
+                Bukkit.setWhitelist(true);
+                new GlobalCommand().global("Whitelist enabled", true);
+            }
+        }, toTicks(12));
+    }
+
+    private void disableLockdownMode()
+    {
+        new AnnounceCommand().announce("Maintenance Mode", "The server is no longer in maintenance mode");
+        new GlobalCommand().global("Whitelist disabled", true);
+        Bukkit.setWhitelist(false);
+    }
+
+    private Long toTicks(int seconds)
+    {
+        return seconds * 20L;
+    }
+}
